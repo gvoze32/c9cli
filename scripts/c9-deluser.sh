8d4af0239d94e19e3d3e6937dbd1c8cde97569e6
Syafa Adena <gvoze32@gmail.com>
2020-06-08 16:22:45 +0700

    fc

--- scripts/c9-deluser.sh
+++ scripts/c9-deluser.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 read -p "Input User : " user
 systemctl stop c9-$user.service
-sudo killall -u $user && sudo deluser --remove-home -f $user
+sudo killall -u $user
+sudo deluser --remove-home -f $user
