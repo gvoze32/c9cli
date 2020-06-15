59b866d6965a23377190ab2bf01a6b96d33db0f5
gvoze32 <gvoze32@gmail.com>
2020-06-15 18:29:49 +0700

    update cd

--- scripts/c9-maker-docker-install.sh
+++ scripts/c9-maker-docker-install.sh
@@ -5,4 +5,5 @@ sudo apt update -y
 sudo apt-get install -y curl git build-essential nodejs npm php php-exif php-gd php-mbstring php-curl php-mysqli php-json php-dom php-fpm python-pip python3-pip python python2.7 python-pyfiglet build-essential zip unzip unp unrar unrar-free unar p7zip apt-transport-https ca-certificates curl gnupg-agent software-properties-common docker docker.io docker-compose
 pip install requests selenium colorama bs4
 sudo adduser c9users
-git clone https://github.com/nicolasjulian/C9-Docker-Compose.git /home/c9users/
+cd /home/c9users
+git clone https://github.com/nicolasjulian/C9-Docker-Compose.git
