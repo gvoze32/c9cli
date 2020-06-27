e91b8884eab5bfeeee581502808b0e42508024ff
gvoze32 <gvoze32@gmail.com>
2020-06-27 18:10:15 +0700

    remove npm from package install lists

--- scripts/firstinstall.sh
+++ scripts/firstinstall.sh
@@ -3,7 +3,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt update -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-sudo apt install -y curl at git build-essential nodejs npm php php-exif php-gd php-mbstring php-curl php-mysqli php-json php-dom php-fpm python-pip python3-pip python python2.7 python-pyfiglet build-essential zip unzip unp unrar unrar-free unar p7zip
+sudo apt install -y curl at git build-essential nodejs php php-exif php-gd php-mbstring php-curl php-mysqli php-json php-dom php-fpm python-pip python3-pip python python2.7 python-pyfiglet build-essential zip unzip unp unrar unrar-free unar p7zip
 pip install requests selenium colorama bs4
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.nvm/nvm.sh
