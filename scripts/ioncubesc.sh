97e05e2a1d4d5ec378305aaf73563c87ca021ced
Syafa Adena <gvoze32@gmail.com>
2020-07-30 23:30:44 +0700

    jerembat

--- scripts/ioncubesc.sh
+++ scripts/ioncubesc.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
-echo Make sure you have PHP 7.2, if not... please exit
-read -r -p "Continue? [y/N] " response
-case "$response" in
-    [yY][eE][sS]|[yY]) 
 wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar -xvzf ioncube_loaders_lin_x86-64.tar.gz
 cd ioncube
-sudo cp ioncube_loader_lin_7.2.so /usr/lib/php/20170718
+echo .
+echo .
+echo .
+ls /usr/lib/php/
+echo .
+echo .
+echo .
+echo Write the PHP extension folder name below
+read -p "Folder Name : " folde
+echo .
+echo .
+echo .
+php -v
+echo Write the php version below
+read -p "Version : " version
+sudo cp ioncube_loader_lin_$version.so /usr/lib/php/$folde
+echo .
+echo .
+echo .
 php -i | grep additional
         ;;
     *)
         :
         ;;
 esac
-
+echo .
+echo .
 echo .
 echo Read text above, if there is cli in it, you have php cli version.
 echo If the text contains fpm, then you have php fpm version.
 echo .
-read -r -p "If you have cli, press Y, or press N if you have fpm" response
+read -r -p "If you have cli, press Y, or press N if you have fpm" :  response
 case "$response" in
     [yY][eE][sS]|[yY]) 
-sudo bash -c 'echo 'zend_extension=ioncube_loader_lin_7.2.so' > /etc/php/7.2/cli/conf.d/00-ioncube-loader.ini'
-service php7.2-fpm restart
+sudo bash -c 'echo 'zend_extension=ioncube_loader_lin_$version.so' > /etc/php/$version/cli/conf.d/00-ioncube-loader.ini'
+service php$version-fpm restart
 php -v
         ;;
     *)
-sudo bash -c 'echo 'zend_extension=ioncube_loader_lin_7.2.so' > /etc/php/7.2/fpm/conf.d/00-ioncube-loader.ini'
-service php7.2-fpm restart
+sudo bash -c 'echo 'zend_extension=ioncube_loader_lin_$version.so' > /etc/php/$version/fpm/conf.d/00-ioncube-loader.ini'
+service php$version-fpm restart
 php -v
         ;;
 esac
