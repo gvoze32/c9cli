ed2d443225ed552b52078826330e7c457413d637
Syafa Adena <gvoze32@gmail.com>
2020-07-30 20:52:07 +0700

    hadeh

--- scripts/c9-deluser-docker.sh
+++ scripts/c9-deluser-docker.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 read -p "Input User : " user
+echo Are the file is using docker or dockermemlimit?
+read -r -p "Answer Y if you are using docker and answer N if you are using dockermemlimit [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+cd /home/c9users
+        ;;
+    *)
+cd /home/c9usersmemlimit
+        ;;
+esac
 sudo docker-compose -p $user down
