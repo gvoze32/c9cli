32e32cc0cf4d01d2c8cbec782666e26b700e9f44
Syafa Adena <gvoze32@gmail.com>
2020-07-31 14:51:53 +0700

    wipe echo

--- scripts/ioncubesc.sh
+++ scripts/ioncubesc.sh
@@ -5,28 +5,16 @@ rm ioncube_loaders_lin_x86-64.tar.gz
 cd ioncube
 echo List of supported php versions:
 ls
-echo .
-echo .
-echo .
 folde="$(command php -i | grep extension_dir 2>'/dev/null' \
     | command head -n 1 \
     | command cut --characters=31-38)"
-echo .
-echo .
-echo .
 version="$(command php --version 2>'/dev/null' \
     | command head -n 1 \
     | command cut --characters=5-7)"
 cp ioncube_loader_lin_${version}.so /usr/lib/php/${folde}
 cd ..
 rm -rf ioncube
-echo .
-echo .
-echo .
 php -i | grep additional
-echo .
-echo .
-echo .
 cat > /etc/php/${version}/cli/conf.d/00-ioncube-loader.ini << EOF
 zend_extension=ioncube_loader_lin_${version}.so
 EOF
