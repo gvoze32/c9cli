2679bc019a1e8c62fcede312ecf548b484a1e42e
Imperial Owl <yogazacty@gmail.com>
2020-07-31 15:06:11 +0700

    update readable variable

--- scripts/ioncubesc.sh
+++ scripts/ioncubesc.sh
@@ -5,17 +5,17 @@ rm ioncube_loaders_lin_x86-64.tar.gz
 cd ioncube
 echo List of supported php versions:
 ls
-folde="$(command php -i | grep extension_dir 2>'/dev/null' \
+php_ext_dir="$(command php -i | grep extension_dir 2>'/dev/null' \
     | command head -n 1 \
     | command cut --characters=31-38)"
-version="$(command php --version 2>'/dev/null' \
+php_version="$(command php --version 2>'/dev/null' \
     | command head -n 1 \
     | command cut --characters=5-7)"
-cp ioncube_loader_lin_${version}.so /usr/lib/php/${folde}
+cp ioncube_loader_lin_${php_version}.so /usr/lib/php/${php_ext_dir}
 cd ..
 rm -rf ioncube
 php -i | grep additional
-cat > /etc/php/${version}/cli/conf.d/00-ioncube-loader.ini << EOF
+cat > /etc/php/${php_version}/cli/conf.d/00-ioncube-loader.ini << EOF
 zend_extension=ioncube_loader_lin_${version}.so
 EOF
 php -v
