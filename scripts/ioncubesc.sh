ae2f42933382b5caaccef0a892d924c2f69dbcf8
Syafa Adena <gvoze32@gmail.com>
2020-07-31 14:49:47 +0700

    full auto

--- scripts/ioncubesc.sh
+++ scripts/ioncubesc.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar -xvzf ioncube_loaders_lin_x86-64.tar.gz
+rm ioncube_loaders_lin_x86-64.tar.gz
 cd ioncube
 echo List of supported php versions:
 ls
 echo .
 echo .
 echo .
-ls /usr/lib/php/
+folde="$(command php -i | grep extension_dir 2>'/dev/null' \
+    | command head -n 1 \
+    | command cut --characters=31-38)"
 echo .
 echo .
 echo .
-echo Write the PHP extension folder name below, example 20170718
-read -p "Folder Name : " folde
-echo .
-echo .
-echo .
-php -v
 version="$(command php --version 2>'/dev/null' \
     | command head -n 1 \
     | command cut --characters=5-7)"
 cp ioncube_loader_lin_${version}.so /usr/lib/php/${folde}
+cd ..
+rm -rf ioncube
 echo .
 echo .
 echo .
