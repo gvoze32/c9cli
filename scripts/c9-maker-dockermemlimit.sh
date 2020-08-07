3e63f7821d8ad5f820970a1fb773a16547a57fff
gvoze32 <gvoze32@gmail.com>
2020-08-07 12:36:33 +0700

    new fft

--- scripts/c9-maker-dockermemlimit.sh
+++ scripts/c9-maker-dockermemlimit.sh
@@ -32,11 +32,6 @@ unzip i-5g.zip
 cd auto_view_story
 npm install
 cd ..
-git clone https://github.com/officialputuid/toolsig
-cd toolsig
-unzip lib.zip
-cd ..
-git clone https://github.com/ikiganteng/toolsigeh
 git clone https://github.com/auliaahmad165/igfeedliker
 git clone https://github.com/areltiyan/igfirstcomment
 cd igfirstcomment
@@ -44,11 +39,8 @@ npm install
 cd ..
 git clone https://github.com/sandrocods/instagram-views
 git clone https://github.com/nthanfp/storyloop
+git clone https://github.com/sanjidtk/sbot
 git clone https://github.com/corrykalam/InstagramAPI
-git clone https://github.com/addrmwn/fft
-cd fft
-unzip node_modules.zip
-cd ..
 mkdir hypervote-v3.2.1-nulled
 cd hypervote-v3.2.1-nulled
 wget https://raw.githubusercontent.com/gvoze32/C9IDECoreDeploy/master/resources/it8C.zip
