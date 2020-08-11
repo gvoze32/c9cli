e78799a6deac63c87d6c793940d789b4391f3c41
gvoze32 <gvoze32@gmail.com>
2020-08-11 14:05:51 +0700

    hvnuld fix res

--- scripts/c9-maker.sh
+++ scripts/c9-maker.sh
@@ -21,9 +21,11 @@ mkdir hypervote-v3.1-official
 cd hypervote-v3.1-official
 wget https://raw.githubusercontent.com/gvoze32/C9IDECoreDeploy/master/resources/i9wX.zip
 unzip i9wX.zip
+rm i9wX.zip
 cd ..
 wget https://raw.githubusercontent.com/gvoze32/C9IDECoreDeploy/master/resources/i-5g.zip
 unzip i-5g.zip
+rm i-5g.zip
 cd auto_view_story
 npm install
 cd ..
@@ -40,21 +42,25 @@ mkdir hypervote-v3.2.1-nulled
 cd hypervote-v3.2.1-nulled
 wget https://raw.githubusercontent.com/gvoze32/C9IDECoreDeploy/master/resources/it8C.zip
 unzip -P sgbteam it8C.zip
+rm it8C.zip
 cd ..
 mkdir hypervote-v3.2.5-nulled
 cd hypervote-v3.2.5-nulled
 wget https://raw.githubusercontent.com/gvoze32/C9IDECoreDeploy/master/resources/iwuh.zip
 unzip -P sgbteambos iwuh.zip
+rm iwuh.zip
 cd ..
 mkdir hypervote-v3.3.2-nulled
 cd hypervote-v3.3.2-nulled
 wget https://raw.githubusercontent.com/gvoze32/C9IDECoreDeploy/master/resources/hjas.zip 
 unzip hjas.zip
+rm hjas.zip
 cd ..
 mkdir hypervote-v3.3.5-nulled
 cd hypervote-v3.3.5-nulled
 wget https://raw.githubusercontent.com/gvoze32/C9IDECoreDeploy/master/resources/jkjf.zip 
 unzip -P sgbshare jkjf.zip
+rm jkjf.zip
 cd
 
 #Get script to user directory
