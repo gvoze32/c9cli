d1f716d34d0c868c3e543b7dcec5c50831351d9a
Syafa Adena <gvoze32@gmail.com>
2020-12-26 14:59:44 +0700

    Create restartdocker.sh

---
+++ restartdocker.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+read -p "Input User : " user
+echo 1. Stop
+echo 2. Start
+echo 3. Restart
+read -r -p "Choose: " response
+case "$restart" in
+    1) 
+echo Are the file is using Docker or Docker Memory Limit?
+echo 1. Docker
+echo 2. Docker Memory Limit
+read -r -p "Choose: " response
+case "$response" in
+    1) 
+cd /home/c9users
+        ;;
+    *)
+cd /home/c9usersmemlimit
+        ;;
+esac
+sudo docker-compose -p $user stop
+        ;;
+    2) 
+echo Are the file is using Docker or Docker Memory Limit?
+echo 1. Docker
+echo 2. Docker Memory Limit
+read -r -p "Choose: " response
+case "$response" in
+    1) 
+cd /home/c9users
+        ;;
+    *)
+cd /home/c9usersmemlimit
+        ;;
+esac
+sudo docker-compose -p $user start
+        ;;
+    *)
+echo Are the file is using Docker or Docker Memory Limit?
+echo 1. Docker
+echo 2. Docker Memory Limit
+read -r -p "Choose: " response
+case "$response" in
+    1) 
+cd /home/c9users
+        ;;
+    *)
+cd /home/c9usersmemlimit
+        ;;
+esac
+sudo docker-compose -p $user stop
+sudo docker-compose -p $user start
+        ;;
+esac
