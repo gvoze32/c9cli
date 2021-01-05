4f6bb3fd7f074ca4b3e3605b561aeff8a5ebb5ae
Syafa Adena <gvoze32@gmail.com>
2021-01-05 12:54:57 +0700

    duplicate command

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install dialog -y
 chmod +x C9IDECoreDeploy/menu/manage.sh
 chmod +x C9IDECoreDeploy/menu/dockermenu.sh
 chmod +x C9IDECoreDeploy/menu/managesystemctl.sh
@@ -23,7 +21,7 @@ sudo apt update -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
 source ~/.profile
 nvm install node
-sudo apt install -y curl at git npm build-essential php php-exif php-gd php-mbstring php-curl php-mysqli php-json php-dom php-fpm python-pip python3-pip python python2.7 python-pyfiglet build-essential zip unzip unp unrar unrar-free unar p7zip dos2unix
+sudo apt install -y dialog curl at git npm build-essential php php-exif php-gd php-mbstring php-curl php-mysqli php-json php-dom php-fpm python-pip python3-pip python python2.7 python-pyfiglet build-essential zip unzip unp unrar unrar-free unar p7zip dos2unix
 pip install requests selenium colorama bs4 wget
 systemctl start atd
 sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common docker docker.io docker-compose
