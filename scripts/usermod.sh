224d09c5e63b34363ef07002813f6142615c5dfb
Syafa Adena <gvoze32@gmail.com>
2021-01-16 15:16:46 +0700

    lolfix

--- scripts/usermod.sh
+++ scripts/usermod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 read -p "Input User : " user
 echo "Input user password"
-passwd user
+passwd $user
 echo "Warning, C9 will be restart!"
-usermod -aG sudo user
+usermod -aG sudo $user
 sudo systemctl daemon-reload
 sudo systemctl enable c9-$user.service
 sudo systemctl restart c9-$user.service
