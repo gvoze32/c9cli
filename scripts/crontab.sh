58adf1b7532a72f74596ffc88367bb4583fc11da
gvoze32 <gvoze32@gmail.com>
2021-03-06 18:09:33 +0700

    rsync backup fixed

--- scripts/crontab.sh
+++
@@ -1,3 +0,0 @@
-echo "Everyday Backup"
-
-0 2 * * * root /root/backup-data-ke-gdrive.sh
--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo "=Everyday Backup at 2 AM="
-echo "Make sure you has been setup a rclone config file using command:"
-echo "rclone config"
+echo "Make sure you has been setup a rclone config file using command: rclone config"
 echo ""
-echo "If all has been set up correctly, then input your rclone remote name"
-read -p "Name : " name
+read -p "If all has been set up correctly, then input your rclone remote name : " name
 sudo cat > /home/backup-$name.sh << EOF
 #!/bin/bash
 date=\$(date +%y-%m-%d)
@@ -12,10 +10,14 @@ rclone mkdir $name:Backup/backup-\$date
 rclone copy /home/ $name:Backup/backup-\$date
 EOF
 chmod +x /home/backup-$name.sh
+echo ""
+echo "Backup command created..."
 crontab -l > backup-$name
 echo "0 2 * * * /home/backup-$name.sh > /home/backup-$name.log 2>&1" >> backup-$name
 crontab backup-$name
 rm backup-$name
-echo "Cron List :"
-crontab -l
 echo ""
+echo "Cron job created..."
+echo ""
+echo "Make sure it's included on your cron list :"
+crontab -l
