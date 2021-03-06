e535d3399c0965d4300d6d27bfc53346d10e289d
gvoze32 <gvoze32@gmail.com>
2021-03-06 17:56:28 +0700

    rsync backup fix

--- misc/backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-date=$(date +%y-%m-%d)
-rclone mkdir $name:Backup/backup-$date
-rclone copy /home/ $name:Backup/backup-$date
--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -5,13 +5,17 @@ echo "rclone config"
 echo ""
 echo "If all has been set up correctly, then input your rclone remote name"
 read -p "Name : " name
-sudo wget https://raw.githubusercontent.com/gvoze32/C9IDECoreDeploy/master/misc/backup.sh -O /home/backup-$name.sh
+sudo cat > /home/backup-$name.sh << EOF
+#!/bin/bash
+date=\$(date +%y-%m-%d)
+rclone mkdir $name:Backup/backup-\$date
+rclone copy /home/ $name:Backup/backup-\$date
+EOF
 chmod +x /home/backup-$name.sh
 crontab -l > backup-$name
 echo "0 2 * * * /home/backup-$name.sh > /home/backup-$name.log 2>&1" >> backup-$name
 crontab backup-$name
 rm backup-$name
-echo "Cron List"
+echo "Cron List :"
 crontab -l
-echo "Backup Executable File, check if it's correct"
-cat /home/backup-$name.sh
+echo ""
