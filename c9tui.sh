b80523e2eaeeff192fc268397dea04bb623f8884
Syafa Adena <gvoze32@gmail.com>
2022-04-01 00:15:49 +0700

    remove new user readme for cli version

--- c9tui.sh
+++ c9tui.sh
@@ -86,37 +86,7 @@ cd /home/$user/my-projects
 ### Your custom default bundling files goes here, it's recommended to put it on resources directory
 ### START
 curl -O https://raw.githubusercontent.com/gvoze32/c9tui/master/scripts/firstinstall.sh
-sudo cat > README.TXT << EOF
-Sebelum memulai, sangat disarankan untuk menginstall PHP, IonCube dan Node.js secara otomatis dengan perintah:
-bash firstinstall.sh
 
-1. Apa spesifikasi workspace yang saya dapat?
-Workspace C9 dibuat menggunakan docker dengan image Debian dengan beberapa konfigurasi limit agar tidak terjadi overload pada server utama
-
-2. Apakah saya dapat user root?
-Mendapatkan user root, kalian dapat menginstall atau merubah konfigurasi sesuka hati
-
-3. Apa yang harus saya lakukan ketika baru mendapatkan workspace?
-Silahkan lakukan update dan upgrade base system kamu agar saling terintegrasi dengan cara menjalankan perintah "apt update" dan "apt upgrade"
-
-4. Bagaimana cara menginstall package?
-Gunakan perintah "apt" atau "apt-get" dilanjutkan dengan nama package yang akan diinstall
-
-5. Bagaimana cara menjalankan program?
-Tergantung menggunakan bahasa apa program tersebut, contohnya:
-
-PHP: php namafile.php
-Node.js: node namafile.js
-Python: python namafile.py
-
-6. Membuka via Android?
-Silahkan gunakan keyboard tambahan Hacker Keyboard atau BeHe Keyboard
-
-7. Bagaimana cara mengatasi error bot WhatsApp?
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
-npm install
-EOF
 ### END
 
 cd
@@ -182,37 +152,7 @@ cd /home/c9users/$user
 ### Your custom default bundling files goes here, it's recommended to put it on resources directory
 ### START
 curl -O https://raw.githubusercontent.com/gvoze32/c9tui/master/scripts/firstinstall.sh
-sudo cat > README.TXT << EOF
-Sebelum memulai, sangat disarankan untuk menginstall PHP, IonCube dan Node.js secara otomatis dengan perintah:
-bash firstinstall.sh
 
-1. Apa spesifikasi workspace yang saya dapat?
-Workspace C9 dibuat menggunakan docker dengan image Debian dengan beberapa konfigurasi limit agar tidak terjadi overload pada server utama
-
-2. Apakah saya dapat user root?
-Mendapatkan user root, kalian dapat menginstall atau merubah konfigurasi sesuka hati
-
-3. Apa yang harus saya lakukan ketika baru mendapatkan workspace?
-Silahkan lakukan update dan upgrade base system kamu agar saling terintegrasi dengan cara menjalankan perintah "apt update" dan "apt upgrade"
-
-4. Bagaimana cara menginstall package?
-Gunakan perintah "apt" atau "apt-get" dilanjutkan dengan nama package yang akan diinstall
-
-5. Bagaimana cara menjalankan program?
-Tergantung menggunakan bahasa apa program tersebut, contohnya:
-
-PHP: php namafile.php
-Node.js: node namafile.js
-Python: python namafile.py
-
-6. Membuka via Android?
-Silahkan gunakan keyboard tambahan Hacker Keyboard atau BeHe Keyboard
-
-7. Bagaimana cara mengatasi error bot WhatsApp?
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
-npm install
-EOF
 ### END
 
 cd
@@ -249,37 +189,7 @@ cd /home/c9usersmemlimit/$user
 ### Your custom default bundling files goes here, it's recommended to put it on resources directory
 ### START
 curl -O https://raw.githubusercontent.com/gvoze32/c9tui/master/scripts/firstinstall.sh
-sudo cat > README.TXT << EOF
-Sebelum memulai, sangat disarankan untuk menginstall PHP, IonCube dan Node.js secara otomatis dengan perintah:
-bash firstinstall.sh
 
-1. Apa spesifikasi workspace yang saya dapat?
-Workspace C9 dibuat menggunakan docker dengan image Debian dengan beberapa konfigurasi limit agar tidak terjadi overload pada server utama
-
-2. Apakah saya dapat user root?
-Mendapatkan user root, kalian dapat menginstall atau merubah konfigurasi sesuka hati
-
-3. Apa yang harus saya lakukan ketika baru mendapatkan workspace?
-Silahkan lakukan update dan upgrade base system kamu agar saling terintegrasi dengan cara menjalankan perintah "apt update" dan "apt upgrade"
-
-4. Bagaimana cara menginstall package?
-Gunakan perintah "apt" atau "apt-get" dilanjutkan dengan nama package yang akan diinstall
-
-5. Bagaimana cara menjalankan program?
-Tergantung menggunakan bahasa apa program tersebut, contohnya:
-
-PHP: php namafile.php
-Node.js: node namafile.js
-Python: python namafile.py
-
-6. Membuka via Android?
-Silahkan gunakan keyboard tambahan Hacker Keyboard atau BeHe Keyboard
-
-7. Bagaimana cara mengatasi error bot WhatsApp?
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb
-npm install
-EOF
 ### END
 
 cd
