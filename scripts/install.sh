dff2463b4f77dc39e5165a82b667584a204eaf71
Syafa Adena <gvoze32@gmail.com>
2022-11-22 04:20:02 +0700

    Fix bug when docker network limit promt

--- scripts/install.sh
+++ scripts/install.sh
@@ -36,10 +36,14 @@ cat > /etc/php/${php_version}/cli/conf.d/00-ioncube-loader.ini << EOF
 zend_extension=ioncube_loader_lin_${php_version}.so
 EOF
 php -v
-read -p "Increase docker network limit more than 30 containers? [Y/n] (Default = n): " REPLY
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    sudo wget https://raw.githubusercontent.com/gvoze32/c9cli/master/scripts/misc/docker-daemon/daemon.json -O /etc/docker/daemon.json
-    service docker restart
-    sudo docker network inspect bridge | grep Subnet
-fi
+read -r -p "Increase docker network limit to more than 30 containers? [y/N] (Default = n): " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        sudo wget https://raw.githubusercontent.com/gvoze32/c9cli/master/scripts/misc/docker-daemon/daemon.json -O /etc/docker/daemon.json
+        service docker restart
+        sudo docker network inspect bridge | grep Subnet
+    ;;
+    *)
+        echo "default docker version set"
+    ;;
+esac
