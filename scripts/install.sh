9b133f193012052a8eea2b89ac18fe97c89a553a
Syafa Adena <gvoze32@gmail.com>
2022-11-22 05:47:20 +0700

    Fix syntax error

--- scripts/install.sh
+++ scripts/install.sh
@@ -36,15 +36,15 @@ cat > /etc/php/${php_version}/cli/conf.d/00-ioncube-loader.ini << EOF
 zend_extension=ioncube_loader_lin_${php_version}.so
 EOF
 php -v
-read -r -p "Increase docker network limit to more than 30 containers? [y/N] (Default = n): " response
-case "$response" in
-    [yY][eE][sS]|[yY]) 
+read -p "Increase docker network limit to more than 30 containers? [y/N] (Default = n): " response
+case ${answer:0:1} in
+    y|Y )
         echo "Setting docker daemon service rule.."
         sudo wget https://raw.githubusercontent.com/gvoze32/c9cli/master/scripts/misc/docker-daemon/daemon.json -O /etc/docker/daemon.json
         service docker restart
         sudo docker network inspect bridge | grep Subnet
     ;;
-    *)
+    * )
         echo "default docker version set"
     ;;
 esac
