3cb65d0f4e2537d271f763fe018508bb78fe357e
Syafa Adena <gvoze32@gmail.com>
2022-12-25 00:36:46 +0700

    update backup

--- c9cli.sh
+++ c9cli.sh
@@ -394,6 +394,13 @@ echo "=Everyday Backup at 2 AM="
 echo "Make sure you has been setup a rclone config file using command: rclone config"
 echo ""
 read -p "If all has been set up correctly, then input your rclone remote name : " name
+echo ""
+echo "Choose the backup service provider"
+echo "1. Google Drive"
+echo "2. Storj"
+read -r -p "Choose: " response
+case "$response" in
+    1) 
 sudo cat > /home/backup-$name.sh << EOF
 #!/bin/bash
 date=\$(date +%y-%m-%d)
@@ -415,6 +422,34 @@ echo "Cron job created..."
 echo ""
 echo "Make sure it's included on your cron list :"
 crontab -l
+        ;;
+    *)
+sudo cat > /home/backup-$name.sh << EOF
+#!/bin/bash
+date=\$(date +%y-%m-%d)
+rclone mkdir $name:backup-\$date
+zip -r /root/backup-\$date.zip /home &
+cpulimit -e zip -l 10
+rclone copy /root/backup-\$date.zip $name:backup-\$date
+rm /root/backup-\$date.zip 
+EOF
+chmod +x /home/backup-$name.sh
+echo ""
+echo "Backup command created..."
+crontab -l > backup-$name
+echo "0 2 * * * /home/backup-$name.sh > /home/backup-$name.log 2>&1" >> backup-$name
+crontab backup-$name
+rm backup-$name
+echo ""
+echo "Cron job created..."
+echo ""
+echo "Make sure it's included on your cron list :"
+crontab -l
+        ;;
+esac
+echo "Backup rule successfully added"
+        ;;
+    *)
 }
 
 backuplites(){
@@ -422,6 +457,13 @@ echo "=Everyday Backup at 2 AM="
 echo "Make sure you has been setup a rclone config file using command: rclone config"
 echo ""
 read -p "If all has been set up correctly, then input your rclone remote name : " name
+echo ""
+echo "Choose the backup service provider"
+echo "1. Google Drive"
+echo "2. Storj"
+read -r -p "Choose: " response
+case "$response" in
+    1) 
 sudo cat > /home/backup-$name.sh << EOF
 #!/bin/bash
 date=\$(date +%y-%m-%d)
@@ -451,6 +493,42 @@ echo "Cron job created..."
 echo ""
 echo "Make sure it's included on your cron list :"
 crontab -l
+        ;;
+    *)
+sudo cat > /home/backup-$name.sh << EOF
+#!/bin/bash
+date=\$(date +%y-%m-%d)
+rclone mkdir $name:backup-\$date
+cd /home
+for i in */; do if ! [[ \$i =~ ^(c9users/|c9usersmemlimit/)$ ]]; then zip -r "\${i%/}.zip" "\$i"; fi done
+cd /home/c9users
+for i in */; do zip -r "\${i%/}.zip" "\$i"; done
+cd /home/c9usersmemlimit
+for i in */; do zip -r "\${i%/}.zip" "\$i"; done
+mkdir /home/backup
+mv /home/*.zip /home/backup/
+mv /home/c9users/*.zip /home/backup/
+mv /home/c9usersmemlimit/*.zip /home/backup/
+rclone copy /home/backup/ $name:backup-\$date/
+rm -rf /home/backup
+EOF
+chmod +x /home/backup-$name.sh
+echo ""
+echo "Backup command created..."
+crontab -l > backup-$name
+echo "0 2 * * * /home/backup-$name.sh > /home/backup-$name.log 2>&1" >> backup-$name
+crontab backup-$name
+rm backup-$name
+echo ""
+echo "Cron job created..."
+echo ""
+echo "Make sure it's included on your cron list :"
+crontab -l
+        ;;
+esac
+echo "Backup rule successfully added"
+        ;;
+    *)
 }
 
 portlist(){
