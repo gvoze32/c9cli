dd0f722fd9f64242aef77d9bcf222c3aa0a9e00e
Syafa Adena <gvoze32@gmail.com>
2024-03-01 07:11:24 +0700

    Try to fix nvm (2)

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,6 +15,8 @@ curl https://rclone.org/install.sh | sudo bash
 
 # Install nvm-nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
 
 # Install dependencies
