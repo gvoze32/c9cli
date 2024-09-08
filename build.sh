4e7924bfdcf22ac341e2900657b6bf34b24ea6be
Syafa Adena <gvoze32@gmail.com>
2024-09-08 08:23:31 +0700

    add intro

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ sudo curl -fsSL https://hostingjaya.ninja/api/c9cli/c9cli -o /usr/local/bin/c9cl
 
 if [ $? -eq 0 ]; then
     echo "c9cli installation successful!"
+    sudo c9cli version
+    echo "Type 'sudo c9cli help' to see the available commands."
 else
     echo -e "\e[31mc9cli installation failed.\e[0m"
 fi
