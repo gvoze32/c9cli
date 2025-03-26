43b42cb5680e47f2842f8f060aff15feb4d8f915
gvoze32 <gvoze32@gmail.com>
2025-03-26 10:07:21 +0700

    fix(docs): Update installation and usage URLs in README and scripts

--- build.sh
+++ build.sh
@@ -15,9 +15,9 @@ echo "
 ==================================================
 "
 
-sudo curl -fsSL https://hostingjaya.ninja/api/mirror/install?raw=true | sudo bash
+sudo curl -fsSL https://hostingjaya.ninja/api/mirror/c9cli/install?raw=true | sudo bash
 
-sudo curl -fsSL https://hostingjaya.ninja/api/mirror/c9cli?raw=true -o /usr/local/bin/c9cli && sudo chmod +x /usr/local/bin/c9cli
+sudo curl -fsSL https://hostingjaya.ninja/api/mirror/c9cli/c9cli?raw=true -o /usr/local/bin/c9cli && sudo chmod +x /usr/local/bin/c9cli
 
 if [ $? -eq 0 ]; then
     echo "c9cli installation successful!"
